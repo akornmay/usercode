86fa0330c9f19107fb618d203b453a79f90e66dc
Andreas Kornmayer <akornmay@pixsim-1.cern.ch>
2014-10-28 16:00:07 +0100

    created scripts to read original run parameters from a csv file updated setup.sh script to use SLC6 libraries

--- sim_ph1pixineff/scripts/setup.sh
+++ sim_ph1pixineff/scripts/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-. /afs/cern.ch/sw/lcg/external/gcc/4.7/x86_64-slc5/setup.sh
-. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.21/x86_64-slc5-gcc47-opt/root/bin/thisroot.sh
+. /afs/cern.ch/sw/lcg/external/gcc/4.7.2p1/x86_64-slc6/setup.sh
+. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.21/x86_64-slc6-gcc47-opt/root/bin/thisroot.sh
